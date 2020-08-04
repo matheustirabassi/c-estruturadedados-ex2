9e0936b74026952f37d9d94d79191414808c0793
Matheus Tirabassi <matheus.tirabassi@aluno.ifsp.edu.br>
2020-08-04 18:25:57 -0300

    addded functions for alloc matrix superior but is wrong

--- main.cpp
+++ main.cpp
@@ -1,9 +1,58 @@
 #include <iostream>
-
+#include <assert.h>
+#include <stdlib.h>
 using namespace std;
 
+
+float **alocMatrixSuperior(int lines)
+{
+    float **p = (float **) malloc(sizeof(float *) * lines);
+    for(int i = 0; i < lines; i++)
+    {
+        p[i] = (float *) malloc(sizeof(float*) *(i + 1) );
+    }
+    return p;
+}
+
+
+void freeMatrixSuperior(float **matrix, int lines)
+{
+    for(int i = 0; i < lines; i++)
+    {
+        free(matrix[i]);
+    }
+    free(matrix);
+
+}
+void printMatrixSuperior(float **matrix, int lines)
+{
+    for(int i = 0; i < lines; i++)
+    {
+        for(int j = 0; j < i + 1; j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+void scanMatrixSuperior(float **matrix, int lines)
+{
+    for(int i = 0; i < lines; i++)
+    {
+        for(int j = 0; j < i + 1; j++)
+        {
+            cout << "Matrix [" << i <<"][" <<j <<"] = ";
+            cin >> matrix[i][j];
+        }
+    }
+}
 int main()
 {
     cout << "Hello world!" << endl;
+    int n = 3;
+    float **matrix = alocMatrixSuperior(n);
+    scanMatrixSuperior(matrix, n);
+    printMatrixSuperior(matrix, n);
+    freeMatrixSuperior(matrix, n);
     return 0;
 }
