d69c2def0537cf2d4c3a8d198e0c5d5df2e24960
Matheus Tirabassi <matheus.tirabassi@aluno.ifsp.edu.br>
2020-08-04 18:51:40 -0300

    matrix triangular incompleted

--- main.cpp
+++ main.cpp
@@ -7,9 +7,18 @@ using namespace std;
 float **alocMatrixSuperior(int lines)
 {
     float **p = (float **) malloc(sizeof(float *) * lines);
+    for(int i = lines; i >= 0; i--)
+    {
+        p[i] = (float *) malloc(sizeof(float*) * (i + 1) );
+    }
+    int aux = lines;
     for(int i = 0; i < lines; i++)
     {
-        p[i] = (float *) malloc(sizeof(float*) *(i + 1) );
+        for(int j = aux - 1 ; j >= 0; j--)
+        {
+            p[i][j] = 0;
+        }
+        aux--;
     }
     return p;
 }
@@ -26,31 +35,36 @@ void freeMatrixSuperior(float **matrix, int lines)
 }
 void printMatrixSuperior(float **matrix, int lines)
 {
+   int aux = lines;
     for(int i = 0; i < lines; i++)
     {
-        for(int j = 0; j < i + 1; j++)
+        for(int j = aux - 1; j >= 0; j--)
         {
             cout << matrix[i][j] << " ";
         }
+        aux --;
         cout << endl;
     }
 }
 void scanMatrixSuperior(float **matrix, int lines)
 {
+    int aux = lines;
     for(int i = 0; i < lines; i++)
     {
-        for(int j = 0; j < i + 1; j++)
+        for(int j = aux - 1; j >= 0; j--)
         {
             cout << "Matrix [" << i <<"][" <<j <<"] = ";
             cin >> matrix[i][j];
         }
+         aux --;
     }
 }
 int main()
 {
     cout << "Hello world!" << endl;
-    int n = 3;
+    int n = 5;
     float **matrix = alocMatrixSuperior(n);
+    printMatrixSuperior(matrix, n);
     scanMatrixSuperior(matrix, n);
     printMatrixSuperior(matrix, n);
     freeMatrixSuperior(matrix, n);
